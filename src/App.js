a14a2694114cf2e5614213d42c77df08f683ff4e
Derek Tam <hi@derekhctam.com>
2022-05-17 09:36:41 -0400

    Mothafucka

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and TEST MOTHAFUCKA MOTHAFUCKA.
         </p>
         <a
           className="App-link"
